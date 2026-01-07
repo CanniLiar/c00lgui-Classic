7adc9d48c024cbdb8498f3c2dfc116f8b39d0c84
CanniLiar <matgonchiara@gmail.com>
2026-01-07 12:01:03 -0300

    Add files via upload

---
+++ c00lgui.lua
@@ -0,0 +1,20 @@
+--[[ ████████      ████      ████     ██ ████     ██ ██ ██         ██      ████      ███████
+    ██            ██  ██     ██ ██    ██ ██ ██    ██ ██ ██         ██     ██  ██     ██    ██
+    ██           ██    ██    ██  ██   ██ ██  ██   ██ ██ ██         ██    ██    ██    ███████
+    ██          ██████████   ██   ██  ██ ██   ██  ██ ██ ██         ██   ██████████   ████
+    ██         ██        ██  ██    ██ ██ ██    ██ ██ ██ ██         ██  ██        ██  ██  ██
+     ████████ ██          ██ ██     ████ ██     ████ ██ ██████████ ██ ██          ██ ██    ██
+     
+    Thanks for using c00lgui Classic!11!!!!111 (Open Source, Edit wathever you want, but gimmie sum credits :) )
+    
+    If you get caugh using this, It's not my fault. I'm not the one making you use this.
+    
+    DO NOT DELETE THIS SCRIPT
+    
+    Clean Version: Under Development.
+]]
+
+--Loader
+
+local GuiScript = loadstring(game:HttpGet("https://", false))
+local Main = loadstring(game:HttpGet("https://", true))
